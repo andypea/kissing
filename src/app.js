fb20716b4b7b10d297b66cf24f725689e9ac8e9b
Andrew Punnett <andrew.punnett@gmail.com>
2016-09-02 23:32:58 +1200

    Add a working application.

--- src/app.js
+++ src/app.js
@@ -1,2 +1,84 @@
-var MatterJs = requre('matter-js')
+var Matter = require('matter-js')
+var Bootstrap = require('bootstrap')
 
+var circleSize = 1;
+var kisses = 0;
+var maxKisses = 0;
+
+// module aliases
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    World = Matter.World,
+    Bodies = Matter.Bodies,
+    Constraint = Matter.Constraint,
+    MouseConstraint = Matter.MouseConstraint,
+    Events = Matter.Events,
+    Composite = Matter.Composite;
+
+// create an engine
+var engine = Engine.create();
+
+engine.world.gravity.y = 0;
+
+// create a renderer
+var render = Render.create({
+    element: document.getElementById("canvasDiv"),
+    engine: engine
+});
+
+// create two boxes and a ground
+var centerCircle = Bodies.circle(400, 300, 80, 80);
+
+centerCircle.isStatic = true;
+
+var mouseConstraint = MouseConstraint.create(engine, {
+  element: render.canvas
+});
+
+Events.on(mouseConstraint, "mousedown", function(event) {
+  var circle = Bodies.circle(event.mouse.absolute.x, event.mouse.absolute.y, 80 * circleSize, 80 * circleSize);
+  var constraint = Constraint.create({
+    pointA: { x: 400, y: 300 },
+    bodyB: circle,
+    pointB: { x: 0, y:0 },
+    length: 1,
+    stiffness: 0.01
+  });
+  World.add(engine.world, [circle, constraint]);
+});
+
+Events.on(engine, "collisionActive", function(event) {
+  kisses = 0; 
+  for (i=0; i < event.pairs.length; i++) {
+    if (event.pairs[i].bodyA.id === centerCircle.id || event.pairs[i].bodyB.id === centerCircle.id) {
+      kisses++;
+    }
+  }
+  if (kisses > maxKisses) {
+    maxKisses = kisses;
+  }
+  document.getElementById("kisses").innerHTML= kisses;    
+  document.getElementById("maxKisses").innerHTML= maxKisses;    
+});
+
+document.getElementById("circleSize").onchange = function() {
+  circleSize = document.getElementById("circleSize").value;
+};
+
+document.getElementById("clearButton").onclick = function() {
+  World.clear(engine.world);
+  World.add(engine.world, centerCircle);
+  kisses = 0;
+  maxKisses = 0;
+  document.getElementById("kisses").innerHTML= kisses;    
+  document.getElementById("maxKisses").innerHTML= maxKisses;    
+};
+
+// add all of the bodies to the world
+World.add(engine.world, [centerCircle, mouseConstraint]);
+
+// run the engine
+Engine.run(engine);
+
+// run the renderer
+Render.run(render);
